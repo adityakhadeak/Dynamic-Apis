a0147d0f74ba1075d46ac238c01cc6e7a8c4609e
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-03-13 18:22:10 +0530

    Update logic added for dyanmic endpoint

--- controllers/dyanmicController.js
+++ controllers/dyanmicController.js
@@ -1,6 +1,69 @@
+const { json } = require('express');
 const pool = require('../database/db.js')
 
-const dynamicController=(req,res)=>{
+const dynamicController = async (req, res) => {
+    try {
+        const { operation, table, column_values } = req.body
+
+
+        switch (operation) {
+            case 'create':
+                // Handle create operation
+                break;
+            case 'read':
+                // Handle read operation
+                break;
+            case 'update':
+                // Handle update operation based on table
+                switch (table) {
+                    case 'interns':
+                        const { intern_id: internId1, full_name, university, start_date, end_date, user_id } = column_values
+                        const updateQuery1 = "UPDATE Interns SET full_name=$1, university=$2, start_date=$3,  end_date=$4 WHERE intern_id=$5 RETURNING *"
+                        const updateValues1 = [full_name, university, start_date, end_date, internId1]
+                        const updateResult1 = await pool.query(updateQuery1, updateValues1);
+
+                        if (updateResult1.rowCount == 0)
+                            return res.status(200).json({
+                                message: "Intern Not Found",
+                            });
+
+                        res.status(200).json({
+                            message: "Data Updated Successfully",
+                            data: updateResult1.rows[0]
+                        });
+                        break;
+                    case 'internshipassignments':
+                        const { assignment_id, intern_id: internId2, task_description, due_date } = column_values
+                        const updateQuery2 = "UPDATE InternshipAssignments SET task_description=$1, due_date=$2 WHERE assignment_id=$3 RETURNING *"
+                        const updateValues2 = [task_description, due_date, assignment_id]
+                        const updateResult2 = await pool.query(updateQuery2, updateValues2);
+
+                        if (updateResult2.rowCount == 0)
+                            return res.status(200).json({
+                                message: "Assignment Not Found",
+                            });
+
+                        res.status(200).json({
+                            message: "Data Updated Successfully",
+                            data: updateResult2.rows[0]
+                        });
+                        break;
+                    default:
+                        res.status(400).json({ message: 'Invalid table name' });
+                        return;
+                }
+                break;
+            case 'delete':
+                // Handle delete operation
+                break;
+            default:
+                res.status(400).json({ message: 'Invalid operation type' });
+                return;
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
 
-module.exports=dynamicController
+module.exports = dynamicController
