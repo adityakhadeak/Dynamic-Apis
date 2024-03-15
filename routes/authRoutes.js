c7cb858e3392eb39493bcc05fad58c73e7efd2df
shaziya78 <125816828+shaziya78@users.noreply.github.com>
2024-03-15 17:23:36 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express =require('express')
 const { loginUser, createUser } = require('../controllers/authRouteController.js')
-
+const {jtoken} =require('../middlewares/authorization.js')
 const routerAuth=express()
 
 routerAuth.post('/register',createUser)
 
-routerAuth.get('/login',loginUser)
+routerAuth.get('/login',jtoken,loginUser)
 
 module.exports=routerAuth
