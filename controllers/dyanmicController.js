fc783a23ab1784a0d00ee477544068b24a2470bb
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-03-15 22:24:37 +0530

    updated dynamic update and read route and added role based authorization for dynamicendpoint update and read

--- controllers/dyanmicController.js
+++ controllers/dyanmicController.js
@@ -4,70 +4,142 @@ const pool = require('../database/db.js')
 const dynamicController = async (req, res) => {
     try {
         const { operation, table, column_values } = req.body
-
+        const { role_id } = req.role_id
 
         switch (operation) {
-             case 'create':
-                    const { full_name, university, start_date, end_date, user_id } = column_values
-                    const createQuery = "INSERT INTO Interns (full_name, university, start_date, end_date, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *"
-                    const createValues = [full_name, university, start_date, end_date, user_id]
-                    const createResult = await pool.query(createQuery, createValues)
-        
-                    if (createResult.rowCount == 0)
-                        return res.status(200).json({
-                            message: "Intern Not Created",
-                        });
-        
-                    res.status(200).json({
-                        message: "Intern Created Successfully",
-                        data: createResult.rows[0]
+            case 'create':
+                const { full_name, university, start_date, end_date, user_id } = column_values
+                const createQuery = "INSERT INTO Interns (full_name, university, start_date, end_date, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *"
+                const createValues = [full_name, university, start_date, end_date, user_id]
+                const createResult = await pool.query(createQuery, createValues)
+
+                if (createResult.rowCount == 0)
+                    return res.status(200).json({
+                        message: "Intern Not Created",
                     });
+
+                res.status(200).json({
+                    message: "Intern Created Successfully",
+                    data: createResult.rows[0]
+                });
                 break;
-                
+
             case 'read':
                 // Handle read operation
                 switch (table) {
-        case 'interns':
-            const readQuery1 = "SELECT * FROM Interns";
-            const readResult1 = await pool.query(readQuery1);
-            res.status(200).json({
-                message: "Data Retrieved Successfully",
-                data: readResult1.rows
-            });
-            break;
-        case 'internshipassignments':
-            const readQuery2 = "SELECT * FROM InternshipAssignments";
-            const readResult2 = await pool.query(readQuery2);
-            res.status(200).json({
-                message: "Data Retrieved Successfully",
-                data: readResult2.rows
-            });
-            break;
-        default:
-            res.status(400).json({ message: 'Invalid table name' });
-            return;
-    }
-    break;
+                    case 'interns':
+                        if (Object.entries(column_values).length == 0) {
+                            if (role_id == 1) {
+
+                                const readQuery1 = "SELECT * FROM Interns";
+                                const readResult1 = await pool.query(readQuery1);
+                                res.status(200).json({
+                                    message: "Data Retrieved Successfully",
+                                    data: readResult1.rows
+                                });
+                            } else {
+                                res.status(403).json({
+                                    message: "Access Denied, Only Admin can view all interns",
+                                });
+                            }
+                        } else {//open to all admin and intern
+                            const { intern_id } = column_values
+                            const readQuery2 = "SELECT * FROM Interns WHERE intern_id=$1";
+                            const readQueryValue2 = [intern_id]
+                            const readResult2 = await pool.query(readQuery2, readQueryValue2);
+                            res.status(200).json({
+                                message: "Data Retrieved Successfully",
+                                data: readResult2.rows[0]
+                            });
+                        }
+                        break;
+                    case 'internshipassignments':
+                        if (Object.entries(column_values).length == 0) {
+                            if (role_id == 1) {//admin only route
+                                const readQuery1 = "SELECT * FROM InternshipAssignments";
+                                const readResult1 = await pool.query(readQuery1);
+                                res.status(200).json({
+                                    message: "Data Retrieved Successfully",
+                                    data: readResult1.rows
+                                });
+                            } else {
+                                res.status(403).json({
+                                    message: "Access Denied, Only Admin can view all internshipassignment",
+                                });
+                            }
+                        } else {//open to all admin and intern
+                            const { intern_id } = column_values
+                            const readQuery2 = "SELECT * FROM InternshipAssignments WHERE intern_id=$1";
+                            const readQueryValue2 = [intern_id]
+                            const readResult2 = await pool.query(readQuery2, readQueryValue2);
+                            res.status(200).json({
+                                message: "Data Retrieved Successfully",
+                                data: readResult2.rows[0]
+                            });
+                        }
+                        break;
+                    default:
+                        res.status(400).json({ message: 'Invalid table name' });
+                        return;
+                }
+                break;
             case 'update':
                 // Handle update operation based on table
                 switch (table) {
                     case 'interns':
+
                         const { intern_id: internId1, full_name, university, start_date, end_date, user_id } = column_values
-                        const updateQuery1 = "UPDATE Interns SET full_name=$1, university=$2, start_date=$3,  end_date=$4 WHERE intern_id=$5 RETURNING *"
-                        const updateValues1 = [full_name, university, start_date, end_date, internId1]
-                        const updateResult1 = await pool.query(updateQuery1, updateValues1);
 
-                        if (updateResult1.rowCount == 0)
-                            return res.status(200).json({
-                                message: "Intern Not Found",
-                            });
+                        const internDataQuery = "SELECT * FROM Intern WHERE intern_id=$1"
+                        const internValue = [internId1]
+                        const internDataResult = await pool.query(internDataQuery, internValue)
 
-                        res.status(200).json({
-                            message: "Data Updated Successfully",
-                            data: updateResult1.rows[0]
-                        });
+                        const internData = internDataResult.rows[0]
+                        if (internData.start_date === start_date && internData.end_date === end_date) {
+                            if (role_id === 2) {//user can update only his info not enddate and start date
+                                const updateQuery1 = "UPDATE Interns SET full_name=$1, university=$2, start_date=$3,  end_date=$4 WHERE intern_id=$5 RETURNING *"
+                                const updateValues1 = [full_name, university, start_date, end_date, internId1]
+                                const updateResult1 = await pool.query(updateQuery1, updateValues1);
+
+                                if (updateResult1.rowCount == 0)
+                                    return res.status(200).json({
+                                        message: "Intern Not Found",
+                                    });
+
+                                res.status(200).json({
+                                    message: "Data Updated Successfully",
+                                    data: updateResult1.rows[0]
+                                });
+                            } else {
+                                res.status(403).json({
+                                    message: "Access Denied, Only Intern can update his info",
+                                });
+                            }
+                        } else if(internData.full_name===full_name && internData.university===university) {
+                            if (role_id === 1) {//admin can update only start and end date and not interns personal info
+                                const updateQuery1 = "UPDATE Interns SET full_name=$1, university=$2, start_date=$3, end_date=$4 WHERE intern_id=$5 RETURNING *"
+                                const updateValues1 = [full_name, university, start_date, end_date, internId1]
+                                const updateResult1 = await pool.query(updateQuery1, updateValues1);
+
+                                if (updateResult1.rowCount == 0)
+                                    return res.status(200).json({
+                                        message: "Intern Not Found",
+                                    });
+
+                                res.status(200).json({
+                                    message: "Data Updated Successfully",
+                                    data: updateResult1.rows[0]
+                                });
+                            }
+                            else{
+                                res.status(403).json({
+                                    message: "Access Denied, Only admin  can update start and end date of internship info",
+                                });
+                            }
+                        }
                         break;
                     case 'internshipassignments':
+                        if(role_id===1){//only admin can update the internshipassignment details 
                         const { assignment_id, intern_id: internId2, task_description, due_date } = column_values
                         const updateQuery2 = "UPDATE InternshipAssignments SET task_description=$1, due_date=$2 WHERE assignment_id=$3 RETURNING *"
                         const updateValues2 = [task_description, due_date, assignment_id]
@@ -82,6 +154,11 @@ const dynamicController = async (req, res) => {
                             message: "Data Updated Successfully",
                             data: updateResult2.rows[0]
                         });
+                    }else{
+                        res.status(403).json({
+                            message: "Access Denied, Only admin  can update internshipassignment info",
+                        });
+                    }
                         break;
                     default:
                         res.status(400).json({ message: 'Invalid table name' });
